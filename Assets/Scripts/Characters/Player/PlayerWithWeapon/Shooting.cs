1978d291c14a68d72c2a11c9bf45c685b66cffe6
Jean Goetten <selbstdenkerdot@hotmail.com>
2023-05-27 00:15:50 -0300

    screen shake system
    adicionado sistema de screen shake quando o personagem atira

--- Assets/Scripts/Characters/Player/PlayerWithWeapon/Shooting.cs
+++ Assets/Scripts/Characters/Player/PlayerWithWeapon/Shooting.cs
@@ -7,6 +7,7 @@ public class Shooting : MonoBehaviour
 {
     public Transform firePoint; 
     public GameObject bulletPrefab; 
+    public new Transform camera; 
 
     public float bulletForce = 20f; 
 
@@ -19,6 +20,7 @@ public class Shooting : MonoBehaviour
             //Shoot Animation 
             
             if(!PauseGame.gameIsPaused){
+                camera.GetComponent<ScreenShake>().start = true; 
                 GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation); 
                 Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>(); 
                 rb.AddForce(firePoint.right * bulletForce, ForceMode2D.Impulse); 
