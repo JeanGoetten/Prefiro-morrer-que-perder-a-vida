2e8ec5bc6e5b6684292e32b3c9ea528d04c75b26
Jean Goetten <selbstdenkerdot@hotmail.com>
2023-06-10 13:36:13 -0300

    loading screen and cut sfx button click length

--- Assets/Scripts/Manager/LoadScene.cs
+++ Assets/Scripts/Manager/LoadScene.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections;
 
@@ -8,15 +9,29 @@ public class LoadScene : MonoBehaviour
     new public AudioSource audio; 
     public AudioClip btn_Start; 
 
+    public GameObject loadingScreen; 
+    public Slider loadingBarFill; 
+
     public void NextScene(string nextSceneName){
+        Time.timeScale = 1;
         audio.Stop(); 
         audio.PlayOneShot(btn_Start); 
         anim.SetTrigger("fadeIn"); 
+
         StartCoroutine(LoadNextScene(nextSceneName)); 
         
     }
     IEnumerator LoadNextScene(string nextSceneName){
+        loadingScreen.SetActive(true); 
         yield return new WaitForSeconds(btn_Start.length); 
-        SceneManager.LoadScene(nextSceneName);
+        AsyncOperation operation = SceneManager.LoadSceneAsync(nextSceneName); 
+        
+
+        while(!operation.isDone){
+            float progressValue = Mathf.Clamp01(operation.progress/0.9f);
+            loadingBarFill.value = progressValue; 
+
+            yield return null;  
+        }   
     }
 }
